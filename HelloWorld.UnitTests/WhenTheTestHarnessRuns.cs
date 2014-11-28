0ac99cf66cbbd2929ef71c95aa2ac991207366cc
Andrew Harcourt <andrewh@uglybugger.org>
2014-11-28 11:45:08 +1000

    When a truck picks up lots of garbage it should get full.

--- HelloWorld.UnitTests/WhenTheTestHarnessRuns.cs
+++ HelloWorld.UnitTests/WhenTheTestHarnessRuns.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
 
 namespace HelloWorld.UnitTests
 {
@@ -8,6 +10,39 @@ namespace HelloWorld.UnitTests
         [Test]
         public void ThePeopleShallRejoice()
         {
+        }
+    }
+
+    public class WhenATruckPicksUpLotsOfGarbage
+    {
+        [Test]
+        public void ItShouldGetFull()
+        {
+            // Arrange
+            var truck = new Truck();
+
+            // Act
+            truck.PickUpGarbage();
+            truck.PickUpGarbage();
+            truck.PickUpGarbage();
+
+            // Assert
+            truck.IsFull.ShouldBe(true);
+        }
+    }
+
+    public class Truck
+    {
+        private int _garbageCollected;
+
+        public void PickUpGarbage()
+        {
+            _garbageCollected++;
+        }
+
+        public bool IsFull
+        {
+            get { return _garbageCollected >= 3; }
         }
     }
 }
