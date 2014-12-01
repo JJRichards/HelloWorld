dfef5ae3eff04c03c7d4b5c5cbd40dc8faf5db8d
Andrew Harcourt <andrewh@uglybugger.org>
2014-12-01 11:07:59 +1000

    Added another dependency to our image capture service. We didn't have any compilation errors to fix :)

--- HelloWorld.App/ImageCaptureService.cs
+++ HelloWorld.App/ImageCaptureService.cs
@@ -6,11 +6,13 @@ namespace HelloWorld.App
     {
         private readonly IClock _clock;
         private readonly ILocationService _locationService;
+        private readonly CaptureSource _captureSource;
 
-        public ImageCaptureService(SystemClock clock, LocationService locationService)
+        public ImageCaptureService(SystemClock clock, LocationService locationService, CaptureSource captureSource)
         {
             _clock = clock;
             _locationService = locationService;
+            _captureSource = captureSource;
         }
 
         public Photo CapturePhoto()
